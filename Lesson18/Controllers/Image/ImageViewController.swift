aa37a1591abfecb89f28a0f0b2d00e4c4913873e
ihValery <ihValery@gmail.com>
2021-03-06 22:31:46 +0300

    fractionCompleted

--- Lesson18/Controllers/Image/ImageViewController.swift
+++ Lesson18/Controllers/Image/ImageViewController.swift
@@ -1,34 +1,42 @@
 import UIKit
 import Alamofire
+import AlamofireImage
 
 class ImageViewController: UIViewController
 {
     private let imageUrl = URLConstants.urlBigImage
-
+    
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var loadingLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        fetchImage()
+        downloadImage()
         isHiddenElements(loadingLabel, activityIndicator, bool: false)
     }
-
-    private func fetchImage()
+    
+    private func downloadImage()
     {
         guard let url = URL(string: imageUrl) else { return }
         
-        AF.request(url).responseImage { [weak self] response in
-            switch response.result {
-                case .success(let image):
-                    isHiddenElements(self!.loadingLabel, self!.activityIndicator, bool: true)
-                    self?.imageView.image = image
-                case .failure(let error):
-                    print(error)
+        AF.request(url)
+            .responseImage { response in
+                switch response.result {
+                    case .success(let image):
+                        isHiddenElements(self.loadingLabel, self.activityIndicator, bool: true)
+                        self.progressView.isHidden = true
+                        self.imageView.image = image
+                    case .failure(let error):
+                        print(error)
+                }
             }
-        }
+            .downloadProgress { progress in
+                let progress = Float(progress.fractionCompleted)
+                self.progressView.setProgress(progress, animated: true)
+            }
     }
 }
 
